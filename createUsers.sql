4a0743cabdadcf1c723671f388587d3b2c793a38
tbabineau <tbabine1@unb.ca>
2025-03-19 11:27:49 -0300

    Changed pwd size to match SHA-512

--- createUsers.sql
+++ createUsers.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
   email varchar(50) NOT NULL,
   fname varchar(50) NOT NULL,
   lname varchar(50) NOT NULL,
-  password_hash varchar(256) NOT NULL,
+  password_hash varchar(128) NOT NULL,
   salt varchar(50) NOT NULL,
   login_attempts INT NOT NULL,
   last_login datetime,
