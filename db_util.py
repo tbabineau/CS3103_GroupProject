f43f57937b6ac4ee8dc9c8075389bbc9296e7de1
tbabineau <tbabine1@unb.ca>
2025-03-19 23:53:08 -0300

    fixing sql call

--- db_util.py
+++ db_util.py
@@ -11,7 +11,7 @@ def getUser(username): #DONT FORGET TO ADD DATA SANITIZATION
             charset='utf8mb4',
             cursorclass= pymysql.cursors.DictCursor)
     cursor = dbConnection.cursor()
-    sql = "SELECT FROM users WHERE username == %s"
-    cursor.execute(sql, (username,))
+    sql = "SELECT * FROM users WHERE username = %s;"
+    cursor.execute(sql, username)
     result = cursor.fetchall()
     return result
