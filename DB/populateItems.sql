ef8673488feca5ca2f8d5ea46eccffcf4b2a0900
Isaac O'Donnell <odonnellisaac2020@gmail.com>
2025-03-21 12:19:07 -0300

    Added PopulateItems to the DB file (NOT COMPLETE)

---
+++ DB/populateItems.sql
@@ -0,0 +1,11 @@
+INSERT INTO storeItems (itemName, itemDescription, itemPrice, itemStock, itemPhoto) VALUES
+('Item1', 'Description for Item 1', 4.99, 8, ''),
+('Item2', 'Description for Item 2', 5.99, 7, ''),
+('Item3', 'Description for Item 3', 3.99, 4, ''),
+('Item4', 'Description for Item 2', 4.99, 4, ''),
+('Item5', 'Description for Item 1', 7.99, 8, ''),
+('Item6', 'Description for Item 2', 2.99, 7, ''),
+('Item7', 'Description for Item 1', 4.99, 6, ''),
+('Item8', 'Description for Item 2', 5.99, 11, ''),
+('Item9', 'Description for Item 1', 6.99, 3, ''),
+('Item10', 'Description for Item 2', 4.99, 5, '');
