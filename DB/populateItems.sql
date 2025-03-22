5d96fade1eb256b352f8bde139e91cf8287b73fc
Isaac O'Donnell <odonnellisaac2020@gmail.com>
2025-03-22 02:08:43 -0300

    Updated populateItems.sql and added images folder ( did not put image fields in the sql file, I wasn't sure how to)

--- DB/populateItems.sql
+++ DB/populateItems.sql
@@ -1,11 +1,6 @@
 INSERT INTO storeItems (itemName, itemDescription, itemPrice, itemStock, itemPhoto) VALUES
-('Item1', 'Description for Item 1', 4.99, 8, ''),
-('Item2', 'Description for Item 2', 5.99, 7, ''),
-('Item3', 'Description for Item 3', 3.99, 4, ''),
-('Item4', 'Description for Item 2', 4.99, 4, ''),
-('Item5', 'Description for Item 1', 7.99, 8, ''),
-('Item6', 'Description for Item 2', 2.99, 7, ''),
-('Item7', 'Description for Item 1', 4.99, 6, ''),
-('Item8', 'Description for Item 2', 5.99, 11, ''),
-('Item9', 'Description for Item 1', 6.99, 3, ''),
-('Item10', 'Description for Item 2', 4.99, 5, '');
+('Minute Maid Orange Juice - 355mL', 'A conveinient way to get your viatmin C. Made with 100% fresh oranges', 4.99, 8, ''),
+('Ocean Spray Cranberry Juice - 1.89L', 'No sugar added, and a delicious source of vitamin c', 5.99, 7, ''),
+('Graves Apple Juice - 945mL', 'Refreshing and pure. Made with 100% real Atlantic apples', 3.99, 4, ''),
+('Simply Fruit Punch - 1.54L', 'A blend of pineapple, cheryand cranberry. Bursting with flavour in every sip', 7.99, 4, ''),
+('ReaLemon Lemon Juice - 125mL', 'Real-fresh lemon juice from concentrate', 2.99, 8, '');
